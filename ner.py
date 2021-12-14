1331a88800926edc9d9315cb0f9f35c3437e5d7c
NaikShweta <snaik11@stevens.edu>
2021-12-14 16:13:45 -0500

    apply word tokenizer

---
+++ ner.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3.9
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+
+ex = 'European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobi	le phone market and ordered the company to alter its practices'
+
+
+def preprocess(sent):
+	sent = nltk.word_tokenize(sent)
+	sent = nltk.pos_tag(sent)
+	return sent
+
+sent = preprocess(ex)
+
+print (sent)
